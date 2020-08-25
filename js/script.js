bc771e4c26162222587b07e7d8dead940d441718
davide-amabile <davideamabile81@gmail.com>
2020-08-25 15:03:56 +0200

    prompt rischiesta cognome

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// 1. Chiedere il cognome
+do {
+  var cognomeUtente = prompt("Inserire il suo cognome");
+} while (!isNaN(cognomeUtente))
+
+// 2. Creare lista array con cognomi
+var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "balsano", "verdi"];
