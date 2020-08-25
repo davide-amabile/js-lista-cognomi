8aa16cd70969392e897b61228bb64f50ce9a6674
davide-amabile <davideamabile81@gmail.com>
2020-08-25 18:12:04 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// 1. Chiedere il cognome
+// 1. Chiedere il cognome e controllare che non inseriscano numeri con il ciclo do/while
 do {
   var cognomeUtente = prompt("Inserire il suo cognome");
 } while (!isNaN(cognomeUtente))
@@ -8,16 +8,16 @@ var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 
 // 3. Inserire cognome utente nella array
 listaCognomi.push(cognomeUtente);
-// console.log(listaCognomi);
 
 //4. Ordinare in ordine alfabetico la lista dei cognomi
 var listaAlafabeto = listaCognomi.sort();
 
+// 5. Stampare a video la lista con il ciclo for
 for ( var i = 0; i < listaAlafabeto.length; i++) {
   var lista = document.getElementById("lista").innerHTML;
   document.getElementById("lista").innerHTML = lista + "<li>" + listaAlafabeto[i] + "</li>";
 }
 
-//5. trovare la posizione del cognomeUtente
+//6. trovare la posizione del cognomeUtente e stampare a video
 var posizioneCognome = listaCognomi.indexOf(cognomeUtente) + 1;
 document.getElementById("posizione").innerHTML = "Il tuo cognome &eacute; nella posizione:" + " " +  posizioneCognome;
